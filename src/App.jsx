4e2a4d1a7b7434e3313f6cca8c64d1ea3cf1c94d
Frjflorezra <franciscojflorezr@gmail.com>
2024-05-30 21:10:44 -0500

    show infor about coffee and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 import { RandomUser } from './components/RandomUser'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CoffeeList } from './components/CoffeeList';
 
 function App() {
   return (
     <>
-      <RandomUser/>
+      <CoffeeList />
     </>
   )
 }
