209efd50209d3354f4e718af18f6277e13b1acae
Frjflorezra <franciscojflorezr@gmail.com>
2024-05-31 19:28:49 -0500

    carousel images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import { RandomUser } from './components/RandomUser'
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { CoffeeList } from './components/CoffeeList';
+import { Beers } from './components/Beers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
-    <>
-      <CoffeeList />
-    </>
+    <div>
+      <header className='container d-flex justify-content-center align-text-center fs-1'>
+        Carousel of Coffee
+      </header>
+      <Beers />
+    </div>
   )
 }
 
--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Carousel } from 'react-bootstrap';
 export const Beers = () => {
-  return (
+  const [imagesList, setImagesList] = useState([]);
 
-    <div>
-      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="..." className="d-block w-100" alt="..."/>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.sampleapis.com/coffee/hot"
+        );
+        setImagesList(response.data);
+        console.log(imagesList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App container-fluid carousel-container">
+      {imagesList.length > 0 ? (
+        <div className="custom-carousel">
+        <Carousel>
+          {imagesList.map((image, index) => (
+            <Carousel.Item key={index}>
+              <img
+                className="d-block w-100"
+                src={image.image}
+                alt={`Slide ${index}`}
+              />
+              <Carousel.Caption>
+                <h5>{image.title}</h5>
+                <p>{image.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-    </div>
-  )
-}
+  );
+};
